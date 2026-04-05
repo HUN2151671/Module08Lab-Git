0855d3e2f59571cbf3d55daed85033d4d563472a
Hunter S <hun2151671@maricopa.edu>
2026-04-04 20:33:34 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Hunter Stevenson 4/4/26
+Module 8 Lab | Part B | Create Repo | CIS256
+
+This is a simple repo
---
+++ test_file.py
@@ -0,0 +1,9 @@
+"""
+Hunter Stevenson 4/4/26
+Module 8 Lab | Part B | Create Repo | CIS256
+
+This is a simple .py file for my own repo
+
+"""
+
+print("Hello World!")
